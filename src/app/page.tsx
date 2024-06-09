6657a08721ef9ae6e36ae71dea5c52f8b14a9f48
Willian Goncalves <williangoncalves74@gmail.com>
2024-06-09 11:18:11 -0300

    rota portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,49 @@ import github from '../../public/img/github.svg';
 import portfolio from '../../public/img/portfolio.svg';
 import software from '../../public/img/software.svg';
 import Link from 'next/link';
+import { Tooltip } from '@mui/material';
 
 export default function Home() {
   return (
     <div className='containerCenter '>
       <div className='containerFlexColCenter'>
         <div className='flex align-middle items-center'>
-          <p className='textPresentation text-white'>
+          <p className='textPresentation text-white '>
             Olá! Eu sou Willian Gonçalves
           </p>
           <div className=' gap-5 hidden md:flex ml-5'>
-            <Link href={'https://github.com/owilliangoncalves'} target='_blank'>
-              <Image src={github} width={45} height={45} alt='Logo Github' />
-            </Link>
-            <Link href={'/portfolio'} target='_blank'>
-              <Image
-                src={portfolio}
-                width={45}
-                height={45}
-                alt='Ícone da tela de um computador simbolizando o portfólio de projetos'
-              />
-            </Link>
-            <Link href={'/software'} target='_blank'>
-              <Image
-                src={software}
-                width={45}
-                height={45}
-                alt='Ícone de uma tag HTML para simbolizar engenharia de software'
-              />
-            </Link>
+            <Tooltip title='GitHub' arrow placement='top'>
+              <Link
+                href={'https://github.com/owilliangoncalves'}
+                target='_blank'
+              >
+                <Image src={github} width={45} height={45} alt='Logo Github' />
+              </Link>
+            </Tooltip>
+            <Tooltip title='Portfólio' arrow placement='top'>
+              <Link href={'/portfolio'}>
+                <Image
+                  src={portfolio}
+                  width={45}
+                  height={45}
+                  alt='Ícone da tela de um computador simbolizando o portfólio de projetos'
+                />
+              </Link>
+            </Tooltip>
+            <Tooltip
+              title='Veja conteúdo sobre Engenharia de Software'
+              arrow
+              placement='top'
+            >
+              <Link href={'/software'}>
+                <Image
+                  src={software}
+                  width={45}
+                  height={45}
+                  alt='Ícone de uma tag HTML para simbolizar engenharia de software'
+                />
+              </Link>
+            </Tooltip>
           </div>
         </div>
         <h1 className='textPresentationImportantUppercase text-white'>
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+export default function Portfolio() {
+  return (
+    <div className='containerFlexColCenter'>
+      <div className='flex align-middle items-center'>
+        <p className='textPresentation text-white '>
+          Olá! Eu sou Willian Gonçalves
+        </p>
+      </div>
+    </div>
+  );
+}
